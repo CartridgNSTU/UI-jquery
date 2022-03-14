259b62b5167d7b598d5a15f385221a46c379483a
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-03-14 13:23:13 +0700

    Zadanie5

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 $(document).ready(function(){
+	$("#but1").click(function () {
 
-	$("#sd1").slider();
-	$("#pb1").progressbar({value:40});
-	$("#tabs").tabs();
-	$("#but1").click(function(){
-	$("#descr1").dialog();
-});
+		$("#container1").effect("bounce",{},1000)
+		$("#container2").effect("pulsate",{}, 1000)
 
-	$("#but2").click(function(){
-		$("#descr2").dialog();
-	});
-	$("#but3").click(function(){
-		$("#descr3").dialog();
 	});
 
-});
+	$("#but2").click(function () {
+
+	$("#container1").hide("explode",{}, 1000)
+	$("#container2").hide("puff",{}, 1000)
+
+	});
+
+	$("#but3").click(function () {
+
+	$("#container1").show("slide",{}, 1000)
+	$("#container2").show("drop",{}, 1000)
+
+	});
+
+	$("#but4").click(function () {
+
+	$("#square ").animate({
+	borderColor: "red",
+	borderWidth: 3,
+	}, 1500);
+
+	});
+
+	$("#but5").click(function () {
+
+	$("#square ").animate({
+	backgroundColor: "brown",
+
+	}, 1500);
+
+	});
+
+	$("#but6").click(function () {
+
+	$("#square ").animate({
+	backgroundColor: "#BEBEBE",
+	borderColor: "black",
+	borderWidth: 1,
+		}, 1500);
+	})
+})
